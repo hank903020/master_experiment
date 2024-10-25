2787dba145d2062d2e9455c54690902a9eb53032
hank903020 <53999035+hank903020@users.noreply.github.com>
2024-10-25 17:00:06 +0800

    Add files via upload

---
+++ experiment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+const int KB_TO_MB = 1024;
+const int BOTTOM_TRACKS = 10240; // bottom tracks¼Æ 20GB
+const int TOP_TRACKS = 10240;    // top tracks¼Æ 20GB
+int main(void)
+{
+
+    return 0;
+}
