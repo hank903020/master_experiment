3b11746ebefd3824644469b4e34914258783a936
hank903020 <53999035+hank903020@users.noreply.github.com>
2025-01-08 15:27:06 +0800

    Update experiment.cpp

--- experiment.cpp
+++ experiment.cpp
@@ -252,7 +252,7 @@ void allocate_SStable(double &latency, int &top_overwrite, int &track_sector, in
 }
 
 // outout
-void write_to_output(const string &filename, double &latency, int &top_overwrite, int i)
+void write_to_output(const string &filename, double &latency, int &top_overwrite, int &top_flag, int &bottom_flag, int i)
 {
     ofstream outfile(filename, ios::app); // 開啟檔案
     if (!outfile.is_open())               // 檢查是否成功開啟
@@ -261,12 +261,14 @@ void write_to_output(const string &filename, double &latency, int &top_overwrite
         return;
     }
     // 換算GB
+    double ii = i;
     int GB = 10;
-    i = i / 160;
-    GB = GB * i;
+    ii = ii / 160;
+    GB = GB * ii;
 
     outfile << "GB: " << GB << endl;
     outfile << "latency: " << latency << "ms" << endl;
+    outfile << "top: " << top_flag << " " << "bottom: " << bottom_flag << endl;
     outfile << "top overwrite: " << top_overwrite << endl
             << endl;
 
@@ -292,7 +294,7 @@ int main(void)
     double latency = 0;                    // write latency
     int top_overwrite = 0;                 // 紀錄top複寫次數
 
-    /******************************************************************************/
+    //******************************************************************************
     int i = 0;
     // 呼叫函式讀取檔案，並將結果存入 level 和 key 陣列中
     readSSTableFile("sstable_info_0.1.txt", level, key);
@@ -301,9 +303,9 @@ int main(void)
     {
         extract_four_sstable(level, key, i, allocat_level, allocat_key); // 提取完4個要寫入sstable
         allocate_SStable(latency, top_overwrite, track_sector, top_flag, bottom_flag, allocat_level, allocat_key, top_tracks, bottom_tracks, top_sstable_level, bottom_sstable_level, top_sstable_key, bottom_sstable_key);
-        if (i != 0 && i % 160 == 0) // 每10GB輸出一次資訊
-            write_to_output("output_file.txt", latency, top_overwrite, i);
+        if (i != 0 && i % 80 == 0) // 每5GB輸出一次資訊
+            write_to_output("output_file.txt", latency, top_overwrite, top_flag, bottom_flag, i);
     }
-    write_to_output("output_file.txt", latency, top_overwrite, i);
-    /*******************************************************************************/
+    write_to_output("output_file.txt", latency, top_overwrite, top_flag, bottom_flag, i);
+    //*******************************************************************************
 }
